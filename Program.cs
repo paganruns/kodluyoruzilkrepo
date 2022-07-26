5de0fb3e11b41d7703f3e9287a345ae654eab81d
SahinZambak <sahinzambak7@gmail.com>
2022-07-26 13:37:51 +0300

    bos

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programlama
+{
+    internal class Program
+    {
+      
+       
+
+        static void Main(string [] args)
+        {
+
+          string o2 = "2";
+          Console.WriteLine(Int32.Parse(o2) + Int32.Parse(o2));
+          Console.ReadLine();
+
+        }
+    }
+}
